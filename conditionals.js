77680c103f07dd44dd5851e6a54b21e94629785c
Ali Bas <alibas01@gmail.com>
2020-12-22 13:34:27 -0500

    some changes on conditionals and while loop

--- conditionals.js
+++ conditionals.js
@@ -1,4 +1,4 @@
-const temperature = -45;
+/*const temperature = -45;
 let raining = false;
 
 if (temperature < 0) {
@@ -17,5 +17,21 @@ if (temperature < -40 || temperature > 40) {
 
 if (!raining) {
   console.log("Leave your umbrella at home!");
-}
+}*/
 
+const whichSchool = function(age) {
+  if (age <13){
+    return "Elementary School";
+  } else if (age>=13&&age<=18){
+    return "Secondary School";
+  } else {
+      return "Lighthouse Labs";
+    }
+  }
+  console.log("I am 35. Which school should I go to?");
+  console.log(whichSchool(35));
+  console.log("I am 8. Which school should I go to?");
+  console.log(whichSchool(8));
+  console.log("I am 14. Which school should I go to?");
+  console.log(whichSchool(14));
+  
